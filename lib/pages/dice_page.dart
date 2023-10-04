1abbbf5ccb3bab54eac845d3611e69d422ae334c
Denis Stahovsky <denis.stahovsky@vantino.ch>
2023-10-04 23:56:44 +0300

    - added dice images; - added provider package; - added first Dice on DicePage.

--- lib/pages/dice_page.dart
+++ lib/pages/dice_page.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:mobx_counter_app/stores/dice_store.dart';
+import 'package:provider/provider.dart';
+
+final DiceStore diceStore = DiceStore();
 
 class DicePage extends StatelessWidget {
   const DicePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final diceStore = Provider.of<DiceStore>(context);
+
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: TextButton(
+                  onPressed: diceStore.roll,
+                  child: Observer(
+                    builder: (_) => Image.asset('images/dice${diceStore.left}.png'),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobx_counter_app/pages/counter_page.dart';
+import 'package:mobx_counter_app/pages/dice_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -24,6 +25,24 @@ class HomePage extends StatelessWidget {
               ),
               child: const Text(
                 "Counter page",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(height: 25),
+            TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
+                padding: const EdgeInsets.all(20),
+              ),
+              onPressed: () => Navigator.of(context).push(
+                  MaterialPageRoute(builder: ((context) => const DicePage()))
+              ),
+              child: const Text(
+                "Dice page",
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                   fontSize: 22,
